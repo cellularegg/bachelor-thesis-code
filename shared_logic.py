0468e398e2e4f6c06dba87cca8730ed2e4e1b7dc
David Zelenay <david.zelenay@tietoevry.com>
2022-04-25 11:57:49 +0200

    added max height to plots

--- shared_logic.py
+++ shared_logic.py
@@ -17,8 +17,10 @@ def set_size(width, fraction=1, subplots=(1, 1)):
     """
     if width == 'thesis':
         width_pt = 364
+        height_pt = 595
     else:
         width_pt = width
+        height_pt = 595
 
     # Width of figure (in pts)
     fig_width_pt = width_pt * fraction
@@ -27,11 +29,13 @@ def set_size(width, fraction=1, subplots=(1, 1)):
 
     # Golden ratio to set aesthetic figure height
     # https://disq.us/p/2940ij3
-    golden_ratio = (5**.5 - 1) / 2
+    golden_ratio = (5 ** .5 - 1) / 2
 
     # Figure width in inches
     fig_width_in = fig_width_pt * inches_per_pt
     # Figure height in inches
-    fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
+    fig_height_in = min(
+        fig_width_in * golden_ratio * (subplots[0] / subplots[1]),
+        height_pt * inches_per_pt)
 
     return (fig_width_in, fig_height_in)
